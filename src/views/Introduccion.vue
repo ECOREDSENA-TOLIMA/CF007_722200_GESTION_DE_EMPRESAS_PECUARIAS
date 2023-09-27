adfa8f900db508a837815047e5d67ee2e66ffbab
DaveRnC <davison.gaitan@gmail.com>
2023-09-27 16:35:03 -0500

    update

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,8 +12,7 @@
 
     figure
     .video(data-aos="fade-right")
-      iframe(width="560" height="315" src="https://www.youtube.com/embed/7ra1cKWHu1M?si=qy48Ijyy7DWXUEWE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen)
-
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/A7EX1D9duG0?si=ltnU-Kp7yf8SWSQg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen)
 </template>
 
 <script>
